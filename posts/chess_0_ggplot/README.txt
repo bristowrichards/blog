9d770e3cb6fb1ee42711edd2df721541320185db
Bristow Richards <57153542+bristowrichards@users.noreply.github.com>
2023-02-02 03:08:57 -0500

    Add first chess post
    data is too big so I'll need to troubleshoot in a bit

---
+++ posts/chess_0_ggplot/README.txt
@@ -0,0 +1,7 @@
+# chess_0_ggplot
+
+The data available in this repository was downloaded from FIDE on January 26th, 2023. It is available in text format on this page:
+
+https://ratings.fide.com/download_lists.phtml
+
+There may have been updates to the data since this post was created.
---
+++ posts/chess_0_ggplot/load_clean_data.R
@@ -0,0 +1,33 @@
+library(readr)
+
+# read data from download
+ratings <- read_fwf(
+  'players_list_foa.txt',
+  col_types = 'icccccccnnnnnnnnnnf',
+  skip=1
+)
+
+# copy columns
+# needed to do this to speed up column type, which required skipping line
+# readr::read_fwf doesn't allow to specify first row as colnames
+columns = c("ID Number", "Name", "Fed", "Sex", "Tit", "WTit", "OTit", 
+            "FOA", "SRtng", "SGm", "SK", "RRtng", "RGm", "Rk", "BRtng", 
+            "BGm", "BK", "Byear", "Flag")
+
+# set column names
+colnames(ratings) <- columns
+rm(columns)
+
+# subset to columns of interest
+ratings <- ratings[,c(1:6,9,12,15,18)]
+
+# clean
+ratings <- ratings |>
+  filter(
+    SRtng > 0, # must have rating and rating must be above 0
+    Byear > 1900 # must have valid birth year
+  ) |>
+  mutate(
+    Bdecade = factor(sapply(Byear, function(x) x - x %% 10)),
+    Age = 2023 - Byear
+  )
